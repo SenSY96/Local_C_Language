63224050dccd131b4ee9d71f230ae14fc1528e4d
Leaf <1678801163@qq.com>
2021-07-14 16:42:38 +0800

    Chapter_2

---
+++ Chapter_2/myfirst.cpp
@@ -0,0 +1,12 @@
+// myfirst.cpp -- display a message
+
+#include <iostream>      // a Preprocessor Directive
+
+int main()                                      // function header
+{                                               // Start of function body
+    using namespace std;                        // make definition visible
+    cout << "Come up and C++ me some time.";    // message
+    cout << endl;                               // start new line
+    cout << "You won't regret it!" << endl;     // more output
+    return 0;                                   // terminate main()
+}                                               // end of function body
